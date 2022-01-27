1fe1cb7a3116941ad510ddab889bf579ca00be5a
MohammadAlHabil <mohammadalhabil1999@gmail.com>
2022-01-27 17:56:11 +0200

    Light and dark mode >relates#8

--- todo.js
+++ todo.js
@@ -36,3 +36,29 @@ let undo = document.getElementById("undo");
 let list = Array.from(document.getElementsByClassName("list"));
 const close = Array.from(document.querySelectorAll(".closee"));
 const model = Array.from(document.querySelectorAll(".bgOfOnBoarding , .addbackground"));
+
+// Light and dark mode
+themeSwitcher.onclick = function () {
+  let dark = Array.from(
+      document.querySelectorAll(
+          ".list , .label , .borderDark , .select-selected , .lineBelow , .below"
+      )
+  );
+  let checkk = Array.from(document.querySelectorAll('input[type="checkbox"]'));
+  let currentTheme = document.documentElement.getAttribute("data-theme");
+  let switchToTheme;
+
+  switchToTheme = currentTheme === "dark" ? "light" : "dark";
+  document.documentElement.setAttribute("data-theme", switchToTheme);
+
+  dark.forEach((el) => {
+      el.classList.toggle("noborder");
+  });
+
+  checkk.forEach((el) => {
+      el.classList.toggle("noborder");
+      if (getComputedStyle(el).border == "0px none rgb(13, 22, 29)")
+          el.style.border = "1px solid #e6e6e6";
+      else el.style.border = "none";
+  });
+};
